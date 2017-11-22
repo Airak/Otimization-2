aba4edd1bd9b79bbc105fdac0033e48f3e87b2b0
Gustavo Borba <gustavohsborba@dri.cefetmg.br>
2017-11-22 18:53:42 -0200

    criacao do cabecalho de estrategias mistas da 1a

---
+++ Questao1/estrategias_mistas.m
@@ -0,0 +1,29 @@
+function [ Pa, Pb, v ] = estrategias_mistas( P )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                Funcao Pn
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Autores:  201212040538 Gustavo Henrique de Souza Borba
+%             201322040095 Marco Antonio do Espirito Santo
+%             201222040107 Matheus Martins Aguiar  
+%             201222040360 Rafael Antunes Batista
+%
+%   Recebe uma matriz que representa uma tabela de premios e retorna, 
+% para cada jogador, o vetor de probabilidades de se selecionar cada estrategia,
+% e o valor do jogo. Em outras palavras, e' uma funcao que resolve o problema 
+% via estrategias mistas.
+%
+%   DESCREVER AQUI COMO IMPLEMENTAMOS ESSA FUNCAO
+%
+%   Parametros: P = Matriz  NxN que representa a tabela de premios
+%
+%   Retorna: Pa = Vetor coluna com a probabilidade de se selecionar cada 
+%                 estrategia para o jogador A
+%            Pb = Vetor coluna com a probabilidade de se selecionar cada 
+%                 estrategia para o jogador B
+%            V = Valor do jogo esperado
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+end

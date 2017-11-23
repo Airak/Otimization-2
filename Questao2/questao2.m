e55350a5ee8a86c498fb52fb928c6f51ccece1e1
MarcoSants <marcochel@gmail.com>
2017-11-22 23:44:40 -0200

    Questao 2

---
+++ Questao2/questao2.m
@@ -0,0 +1,43 @@
+function [ out ] = questao2()
+contZero = 0;
+contQuatro = 0;
+
+for i = 1:1000
+    estado = 1;
+    for j = 1:100
+        if estado == 0 
+            contZero = contZero + 1; 
+            break;
+        elseif estado == 1
+            rand = randi([1, 3]);
+            if rand == 1
+                estado = 2;
+            else
+                estado = 0;
+            end
+        elseif estado==2
+            rand = randi([1, 3]);
+            if rand == 1
+                estado = 3;
+            else
+                estado = 1;
+            end
+        elseif estado==3
+            rand = randi([1, 3]);
+            if rand == 1
+                estado = 4;
+            else
+                estado = 3;
+            end
+        else
+            contQuatro = contQuatro + 1;
+            break;
+        end
+    end
+end
+
+percZero = (contZero * 100)/(contZero + contQuatro)
+percQuatro = (contQuatro * 100)/(contZero + contQuatro)
+fprintf("Probabilidade da cadeia cair no estado Zero: %.2f", percZero);
+fprintf("\nProbabilidade da cadeia cair no estado Quatro: %.2f", percQuatro);
+end
